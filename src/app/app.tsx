df47947de912a956562351b5f203b41cbe65e871
Andrey Popov <dil2232@gmail.com>
2025-08-12 19:39:14 +0600

    Initial commit
    Set up:
    - NX workspace and React
    - RadixUI
    - Tailwind CSS
    - MobX

---
+++ src/app/app.tsx
@@ -0,0 +1,11 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return <p className="text-red-600">123</p>;
+}
+
+export default App;
